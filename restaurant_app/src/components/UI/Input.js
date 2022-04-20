096266d1ce0c97be3a7af5956dbc23adf436b2c6
Johnson <ktjohnson@loaner5033.fullsail.com>
2022-04-20 12:37:22 -0400

    working on the mealItemForm

--- restaurant_app/src/components/UI/Input.js
+++ restaurant_app/src/components/UI/Input.js
@@ -1,12 +1,17 @@
+import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+// You get the ref which can now be set through the ref prop on your Component,
+// as a second parameter here, and you can now use it inside of your Component
+// function to forward that ref to that input here
+
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;

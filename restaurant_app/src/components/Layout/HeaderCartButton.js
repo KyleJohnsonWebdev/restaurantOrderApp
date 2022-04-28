26112d2641194e8622d0c6c7f39dbc0e8b93596f
Johnson <ktjohnson@loaner5033.fullsail.com>
2022-04-28 09:50:37 -0400

    got the bump animation completed

--- restaurant_app/src/components/Layout/HeaderCartButton.js
+++ restaurant_app/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,43 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
-  // transform an array into single number. First
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+  // I will not always add the bump class, but I'll check if btnIsHighlighted
+  // is true. And if that's the case, classes.bump is added, otherwise an empty string is added
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+  // I pass our effect function to that and then our array of dependencies
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    // set a timer which removes that class again so that when it's added again in the future, it again plays
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    // In useEffect, this will be called automatically as a cleanup function by React. And here, I'll call clearTimeout
+    // and clear that timer, so that when this effect reruns, we clear the timer.
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
@@ -19,4 +46,5 @@ const HeaderCartButton = (props) => {
     </button>
   );
 };
+
 export default HeaderCartButton;
